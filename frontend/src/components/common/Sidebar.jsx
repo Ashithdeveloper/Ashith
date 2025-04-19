b6f33cffcc7bb6e7e2e882adea8b605b57f8265c
Ashithdeveloper <160271719+Ashithdeveloper@users.noreply.github.com>
2025-04-19 12:31:28 +0530

    Update Sidebar.jsx

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -39,7 +39,7 @@ const Sidebar = () => {
   const { data: authUser } = useQuery({ queryKey: ["authUser"] });
 
   return (
-    <div className="md:flex-[2_2_0] w-18 max-w-58 ml-5 hidden sm:block ">
+    <div className="md:flex-[2_2_0] w-18 max-w-60 ml-5 hidden sm:block ">
       <div className="sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-20 md:w-full">
         <Link to="/" className="flex justify-center md:justify-start h-25   ">
           <div className=" mt-5 font-bold text-4xl ml-1.5   rounded-2xl hidden lg:block mr-4 ">
